ae60b63f91f5c05046f539b1f41b10220cfa3fca
KateRin-KA <Kuska2006@list.ru>
2024-10-07 22:03:13 +0300

    Update module_2_2.py

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first=int(input("Ввелите число 1"))
+second=int(input("Ввелите число 2"))
+third=int(input("Ввелите число 3"))
+if first==second and first==third:
+     print(3)
+else:
+    if first == second or first == third or second==third:
+        print(2)
+        if first != second and first != third and second != third:
+            print(0)
